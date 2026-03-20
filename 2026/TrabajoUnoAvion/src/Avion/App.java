e91ebc6a4d5f3d2e9b2100ff80c6a16ee0132e78
peerre <lautaroprojas@gmail.com>
2026-03-20 11:58:25 -0300

    Añadir Sistema de Vuelo al Main

--- 2026/TrabajoUnoAvion/src/Avion/App.java
+++ 2026/TrabajoUnoAvion/src/Avion/App.java
@@ -2,7 +2,9 @@ package Avion;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Al muuuundoooo!!");
+
+        //----------------- ALAS ---------------
 
         Ala alaIzquierda = new Ala(10.5 , "Blanco" , "Aluminio");
         Ala alaDerecha = new Ala(10.5 , "Blanco", "Aluminio");
@@ -17,6 +19,22 @@ public class App {
 
         System.out.println("Ala derecha:");
         System.out.println(alaDerecha);
-    }
 
+        System.out.println("\n====================\n");
+
+        //----------------- Sistema De Control de Vuelo ---------------
+
+        SistemaDeControlDeVuelo sistemaBoeing = new SistemaDeControlDeVuelo("Boeing", 5 , TipoDeSistema.PilotoAutomatico);
+
+        SistemaDeControlDeVuelo sistemaAirbus = new SistemaDeControlDeVuelo("Airbus", 8, TipoDeSistema.Manual);
+
+        //Imprimir la Información de los sistemas
+        System.out.println("---Sistema Boeing---");
+        System.out.println("Fabricante: " + sistemaBoeing.getFabricante());
+        System.out.println("Modo de Vuelo Actual: " + sistemaBoeing.getModoActual());
+
+        System.out.println("---Sistema Airbus---");
+        System.out.println("Fabricante: " +sistemaAirbus.getFabricante());
+        System.out.println("Modo de Velo Actual: " + sistemaAirbus.getModoActual());
+    }
 }
