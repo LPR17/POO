f1885e296e95308919d8b2b505b8587c9a59bc95
lautypr <lautaroprojas@gmail.com>
2026-05-31 16:59:48 -0300

    Creacion paquetes del proyecto

--- 2026/TPO/SistemaGestionFestivalCine/src/Modelo/Sala/Sala.java
+++ 2026/TPO/SistemaGestionFestivalCine/src/Modelo/Sala/Sala.java
@@ -1,4 +1,21 @@
 package Modelo.Sala;
 
 public class Sala {
+
+    //Atributos
+    private String  nombre;
+    private String ubicacion;
+    private int capacidad;
+    private List<Butaca> butacas;
+
+    //Constructor
+    public Sala (String nombre, String ubicacion, int capacidad) {
+        if (nombre == null | nombre.isBlank()){
+            throw new DatosInvalidosException("El nombre de la sala no puede estar vacío");
+        }
+
+
+
+
+    }
 }
